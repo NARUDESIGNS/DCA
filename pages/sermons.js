0a37317002a91fe62f44c68b6d9034c0198efc8f
heyt0pe <akinthope@gmail.com>
2023-05-27 16:56:39 +0100

    sermon search

--- pages/sermons.js
+++ pages/sermons.js
@@ -1,28 +1,36 @@
+const sermons = document.getElementById('sermons');
+const search = document.getElementById('search');
+
+let allSermonsJSON = []
+
 async function getSermons() {
     const request = await fetch('https://divinechristianassembly.com/api/dca_api/index.php/sermons');
     const response = await request.json();
+    allSermonsJSON = response.data;
+    filterSermons();
+}
+
+function filterSermons() {
     let allSermons = '';
-    for (const sermon of response.data) {
-        let sermonDate = new Date(sermon.date);
-        let sermonDiv = `
-        <div class='sermon'>
-            <audio controls>
-            <source src='${sermon.sermon}'>
-            </audio>
-            <p class='title'>${sermon.title}</p>
-            <div class='sermon-row'>
-                <p class='preacher'>${sermon.preacher}</p>
-                <p class='date'>${sermonDate.toDateString()}</p>
+    for (const sermon of allSermonsJSON) {
+        if (sermon.title.toLowerCase().includes(search.value.toLowerCase())) {
+            let sermonDate = new Date(sermon.date);
+            let sermonDiv = `
+            <div class='sermon'>
+                <audio controls>
+                <source src='${sermon.sermon}'>
+                </audio>
+                <p class='title'>${sermon.title}</p>
+                <div class='sermon-row'>
+                    <p class='preacher'>${sermon.preacher}</p>
+                    <p class='date'>${sermonDate.toDateString()}</p>
+                </div>
             </div>
-        </div>
-        `;
-        allSermons += sermonDiv;
+            `;
+            allSermons += sermonDiv;
+        }
     }
-    const sermons = document.getElementById('sermons');
-    console.log(sermons);
     sermons.innerHTML = allSermons;
-
-
 }
 
 getSermons();
